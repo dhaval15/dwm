6ccf16f1f4e62663eea114d8c51293398a2d90f0
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-20 00:10:27 +0530

    notitle

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "SauceCodePro Nerd Font:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,12 +21,13 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
 	"nitrogen","--restore",NULL,
-	"sh", "-c", "while :; do dwm_status -; sleep 1; done", NULL,
+	"dwmblocks",NULL,
+	//"sh", "-c", "while :; do dwm_status -; sleep 1; done", NULL,
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", ""};
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", " "};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -122,7 +123,6 @@ static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
