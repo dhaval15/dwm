a733ab36c081912b923a99be56bca6a6183d620e
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-20 17:27:47 +0530

    Swallowing

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SauceCodePro Nerd Font:size=9" };
@@ -34,10 +35,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class         instance    title       tags mask   switchtotag    isfloating   monitor */
-	{ "Alacritty",   NULL,       NULL,       1 << 0,     1,              0,           -1 },
-	{ "Gimp",        NULL,       NULL,       1 << 6,     1,              0,           -1 },
-	{ "firefox",     NULL,       NULL,       1 << 1,     1,              0,           -1 },
+	/* class         instance    title       tags mask   switchtotag    isfloating   terminal  noswallow  monitor */
+	{ "Alacritty",   NULL,       NULL,       1 << 0,     1,              0,          1, 	   1,	      -1 },
+	{ "Gimp",        NULL,       NULL,       1 << 6,     1,              0,          0,        1,         -1 },
+	{ "firefox",     NULL,       NULL,       1 << 1,     1,              0,          0,        0,         -1 },
 };
 
 /* layout(s) */
